bef2d32c350de272bae06a85c3beff3f1b203ed0
Nick Holt <nholt@doeanderson.com>
2018-03-12 10:53:08 -0400

    Added error handling for cancelled PGA events

--- sportradaR.R
+++ sportradaR.R
@@ -49,29 +49,80 @@ getGolfTournamentLeaderboard <- function(id, year, tour = "pga") {
         start_url <- paste0("http://api.sportradar.us/golf-", access_level, "2", "/leaderboard/", tour,"/", year, "/tournaments/", id, "/leaderboard.json?api_key=", golf_key)
         targetJSON <- getURL(start_url)
         target <- fromJSON(targetJSON)
-        df <- data.frame(target$leaderboard)
-        player_rounds <- NULL
-        for(i in 1:length(df$rounds)){
-                scores <- data.frame(df$rounds[[i]]) %>%
-                        select(strokes, sequence) %>%
-                        spread(sequence, strokes)
-                score_df <- NULL
-                score_df$one <- ifelse(is.null(scores$`1`) == FALSE, scores$`1`, NA)
-                score_df$two <- ifelse(is.null(scores$`2`) == FALSE, scores$`2`, NA)
-                score_df$three <- ifelse(is.null(scores$`3`) == FALSE, scores$`3`, NA)
-                score_df$four <- ifelse(is.null(scores$`4`) == FALSE, scores$`4`, NA)
-                player_rounds <- rbind(player_rounds, score_df)
+        if(target$status == "cancelled") {
+                target_df <- NULL
+                target_df$id <- id
+                target_df$player_id <- "cancelled"
+        } else {
+                df <- data.frame(target$leaderboard)
+                player_rounds <- NULL
+                for(i in 1:length(df$rounds)){
+                        scores <- data.frame(df$rounds[[i]]) %>%
+                                select(strokes, sequence) %>%
+                                spread(sequence, strokes)
+                        score_df <- NULL
+                        score_df$one <- ifelse(is.null(scores$`1`) == FALSE, scores$`1`, NA)
+                        score_df$two <- ifelse(is.null(scores$`2`) == FALSE, scores$`2`, NA)
+                        score_df$three <- ifelse(is.null(scores$`3`) == FALSE, scores$`3`, NA)
+                        score_df$four <- ifelse(is.null(scores$`4`) == FALSE, scores$`4`, NA)
+                        player_rounds <- rbind(player_rounds, score_df)
+                }
+                player_rounds <- data.frame(player_rounds)
+                colnames(player_rounds) <- str_c("round_", colnames(player_rounds), "_strokes")
+                target_df <- cbind(df %>% select(-rounds), player_rounds)
+                colnames(target_df)[which(colnames(target_df)=="id")] <- "player_id"
+                tourn_id <- id
+                target_df <- target_df %>%
+                        mutate(tourn_id = tourn_id,
+                               tourn_year = as.integer(year),
+                               tourn_tour = tour) %>%
+                        select(tourn_id, tourn_year, tourn_tour, everything()) %>%
+                        mutate_if(is.list, as.numeric)
         }
-        player_rounds <- data.frame(player_rounds)
-        colnames(player_rounds) <- str_c("round_", colnames(player_rounds), "_strokes")
-        target_df <- cbind(df %>% select(-rounds), player_rounds)
-        colnames(target_df)[which(colnames(target_df)=="id")] <- "player_id"
-        tourn_id <- id
-        target_df <- target_df %>%
-                mutate(tourn_id = tourn_id,
-                       tourn_year = year,
-                       tourn_tour = tour) %>%
-                select(tourn_id, tourn_year, tourn_tour, everything())
         return(target_df)
 }
 
+# function for computing cumulative sd over time series such as tournament leaderboard     
+cum_sd <- function(x){
+        sds <- NULL
+        for(i in seq_along(x)){
+                y <- x[1:i][!is.na(x[1:i])]
+                if(length(y) > 1) {
+                        sds[i] <- sqrt((sum((y-mean(y, na.rm = T))^2))/(length(y)-1))
+                } else {
+                        sds[i] <- NA
+                }
+        }
+        return(sds)
+}
+
+# function to sum variables while ignoring NAs
+sum_vars <- function(x, y, ...) {
+        vec <- c(x, y, ...)
+        sum(vec[!is.na(vec)])
+}
+
+# function to update dataframe of tournament leaderboards with info from recently completed competitions
+
+        # this function takes a df of leaderboard data that has been joined to tournament info data        
+
+updateGolfLeaderboard <- function(tourn_df, tourn_list) {
+        last_tournament_start <- max(tourn_df$start_date)
+        last_tournament_end <- max(tourn_df$end_date)
+        recent_tournaments <- tourn_list %>% 
+                filter(start_date > last_tournament_start & end_date < today())
+        if(length(recent_tournaments$id)==0) {
+                print("No recent tournaments. Your tournament df is up to date.")
+        } else {
+                tourn_df <- tourn_df %>%
+                        select(tourn_id:round_four_strokes) %>%
+                        mutate_if(is.list, as.numeric)
+                for(i in seq_along(recent_tournaments$id)){
+                        lb <- getGolfTournamentLeaderboard(id = recent_tournaments$id[i], year = as.character(year(recent_tournaments$start_date[i])))
+                        updated_scores <- bind_rows(tourn_df, lb)
+                        Sys.sleep(1)
+                }
+                return(updated_scores)
+        }
+        
+}
